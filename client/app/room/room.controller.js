5327407ba0a934291349f4af84232fbe202c2e8b
Roman Bartusiak <roman.bartusiak@pwr.edu.pl>
2016-01-09 22:47:14 +0100

    adding old text handling

--- client/app/room/room.controller.js
+++ client/app/room/room.controller.js
@@ -1,24 +1,28 @@
 'use strict';
 
 angular.module('megafon')
-  .controller('RoomCtrl', ['$scope','io','$stateParams','$state',function ($scope,io,$stateParams,$state) {
-  	io.emit('join_room',{'roomName':$stateParams.roomName});
+.controller('RoomCtrl', ['$scope','io','$stateParams','$state',function ($scope,io,$stateParams,$state) {
+ io.emit('join_room',{'roomName':$stateParams.roomName});
 
-  io.on('room_dont_exsists',function(){
-  	$state.go('main');
-  });
-  	io.on('in_room',function(data){
-		$scope.canWrite=data.canWrite;
-		$scope.$digest();
-	});
-  	$scope.text='';
-  	$scope.update=function(text,fontSize){
-  		io.emit('write',{text:text,fontSize:fontSize});
-  	};
-  	$scope.canWrite=false;
-  	io.on('text_update',function(data){
-		$scope.text=data.text;
-		$scope.fontSize=isNaN(parseFloat(data.fontSize))?72:data.fontSize;
-		$scope.$digest();
-  	});
-  }]);
+ io.on('room_dont_exsists',function(){
+   $state.go('main');
+ });
+ io.on('in_room',function(data){
+  if(data.text){
+    $scope.text=data.text.text;
+    $scope.fontSize=isNaN(parseFloat(data.text.fontSize))?72:data.text.fontSize;
+  }
+  $scope.canWrite=data.canWrite;
+  $scope.$digest();
+});
+ $scope.text='';
+ $scope.update=function(text,fontSize){
+  io.emit('write',{text:text,fontSize:fontSize});
+};
+$scope.canWrite=false;
+io.on('text_update',function(data){
+  $scope.text=data.text;
+  $scope.fontSize=isNaN(parseFloat(data.fontSize))?72:data.fontSize;
+  $scope.$digest();
+});
+}]);
